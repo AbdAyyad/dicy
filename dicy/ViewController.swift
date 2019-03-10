2fe1d069b37d1feefb1794aa8765316dd5ce6116
Abdelrahman Ayyad <abdelrahmanayyad@Abdelrahmans-iMac.local>
2019-03-10 07:49:52 -0700

    Initial Commit

---
+++ dicy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dicy
+//
+//  Created by Abdelrahman Ayyad on 3/10/19.
+//  Copyright © 2019 Abdelrahman Ayyad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
